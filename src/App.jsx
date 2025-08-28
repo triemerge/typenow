82b3520bb6da98d902f91ea95b8ec79931dbdda0
Aditya Kumar Gupta <aditya.rakesh.2005@gmail.com>
2025-08-28 16:57:00 +0530

    add sonner toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
@@ -9,6 +10,7 @@ import NotFound from "./NotFound";
 const App = () => (
   <ThemeProvider>
     <Toaster />
+    <Sonner />
     <div className="min-h-screen bg-background text-foreground flex flex-col">
       <Header />
       <BrowserRouter>
