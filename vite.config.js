a7c8fa50c20520e877e60973b12ad1060a9a251a
Aditya Kumar Gupta <aditya.rakesh.2005@gmail.com>
2025-09-22 23:08:00 +0530

    improve vite build config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
   },
 });
